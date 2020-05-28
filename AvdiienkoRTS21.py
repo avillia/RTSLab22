142d6a8a591312e756d828da3314721e52031923
Illia Avdiienko <avil48job@gmail.com>
2020-05-28 20:40:32 +0300

    Update AvdiienkoRTS21.py

--- AvdiienkoRTS21.py
+++ AvdiienkoRTS21.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 
 def dft(Y):
-    turning_coefficients_R = [[0 for elem in dots] for elem in dots]
-    turning_coefficients_I = [[0 for elem in dots] for elem in dots]
+    turning_coefficients_R = [[0 for _ in dots] for _ in dots]
+    turning_coefficients_I = [[0 for _ in dots] for _ in dots]
 
     for x in dots:
         for y in dots:
@@ -24,6 +24,26 @@ def dft(Y):
     dft_magnitude_I = turning_coefficients_I.dot(Y)
 
     results_for_plotting = [math.sqrt(dft_magnitude_I[index] ** 2 + dft_magnitude_R[index] ** 2) for index in dots]
+    return results_for_plotting
+
+
+def table_dft(Y):
+    turning_coefficients_R = [[0 for _ in dots] for _ in dots]
+    turning_coefficients_I = [[0 for _ in dots] for _ in dots]
+    Fp2_real = [0] * 1024
+    Fp2_image = [0] * 1024
+
+    for x in dots:
+        for y in dots:
+            turning_coefficients_R[x][y] = math.cos(2 * math.pi / Ω_max * x * y)
+            turning_coefficients_I[x][y] = math.sin(2 * math.pi / Ω_max * x * y)
+
+    for i in range(len(turning_coefficients_R)):
+        for j in range(len(turning_coefficients_R[i])):
+            Fp2_real[i] += Y[j] * turning_coefficients_R[i][j]
+            Fp2_image[i] += Y[j] * turning_coefficients_I[i][j]
+
+    results_for_plotting = [(r ** 2 + i ** 2) ** 0.5 for (r, i) in zip(Fp2_real, Fp2_image)]
     return results_for_plotting
 
 
@@ -50,10 +70,16 @@ if __name__ == '__main__':
     dft_start = datetime.now()
     dft_results = dft(Y=mainX)
     dft_finish = datetime.now()
-    print(f"FFT time of execution (non-parallel):\n{dft_finish - dft_start} ms.")
+    print(f"DFT time of execution:\n{dft_finish - dft_start} ms.")
+
+    table_dft_start = datetime.now()
+    table_dft_results = table_dft(Y=mainX)
+    table_dft_finish = datetime.now()
+    print(f"Table DFT time of execution:\n{table_dft_finish - table_dft_start} ms.")
 
     # Results plotting
 
-    fig, (dft_plot) = plt.subplots(1)
+    fig, (dft_plot, table_dft_plot) = plt.subplots(2, 2)
     dft_plot.plot(dft_results)
+    table_dft_plot.plot(table_dft_results)
     fig.show()
